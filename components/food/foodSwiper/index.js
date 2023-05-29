fa5928273befe44868316d5367ba7cf835288154
Red-misst <109917068+Red-misst@users.noreply.github.com>
2023-05-29 13:02:35 +0000

    style(components): adjust food swiper and food card styles

--- components/food/foodSwiper/index.js
+++ components/food/foodSwiper/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper";
+
+import { Autoplay, EffectCoverflow, Pagination } from "swiper";
 
 export default function FoodSwipero({ setLoading }) {
   const handleLinkClick = () => {
@@ -13,19 +15,25 @@ export default function FoodSwipero({ setLoading }) {
   return (
     <>
       <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
+        effect={"coverflow"}
+        grabCursor={true}
         loop={true}
-        pagination={{
-          clickable: true,
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
         }}
+        pagination={true}
         autoplay={{
           delay: 2000,
           disableOnInteraction: false,
         }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mainSwiper"
+        modules={[Autoplay, EffectCoverflow, Pagination]}
+        className="foodSwiper"
       >
         {[...Array(10).keys()].map((i) => (
           <SwiperSlide>
